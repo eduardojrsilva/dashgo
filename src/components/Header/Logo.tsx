fda4526077772f1faf5ed442878725f4a9eafe70
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-05-03 09:06:32 -0300

    adjusting responsiveness

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export function Logo() {
   return (
     <Text
-      fontSize="3xl"
+      fontSize={["2xl", "3xl"]}
       fontWeight="bold"
       letterSpacing="tight"
       w="64"
@@ -12,4 +12,4 @@ export function Logo() {
       <Text as="span" ml="1" color="pink.500">.</Text>
     </Text>
   );
-}
+}         
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Eduardo Silva</Text>
-        <Text color="gray.300" fontSize="small">eduardojrodriguessilva@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Eduardo Silva</Text>
+          <Text color="gray.300" fontSize="small">eduardojrodriguessilva@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Eduardo Silva" src="https://github.com/eduardojrsilva.png" />
     </Flex>
