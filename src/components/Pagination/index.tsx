312c9438c7ec896d1ed8d3b559577fa527f91977
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-05-30 16:54:34 -0300

    adjusting total count message

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -42,7 +42,11 @@ export function Pagination({
       align="center"
     >
       <Box>
-        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+        <strong>{registersPerPage * currentPage - registersPerPage + 1}</strong>
+        {` - `}
+        <strong>{registersPerPage * currentPage}</strong>
+        {` de `}
+        <strong>{totalCountOfRegisters}</strong>
       </Box>
 
       <Stack direction="row" spacing="2">
